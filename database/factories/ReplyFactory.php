97d8b90b68d0b72461e78cd861d18c8dfaf7227d
aleksej <aleksej.shherbak@yandex.ru>
2019-05-10 02:26:55 +0300

    Initial commit. Prepared db seeders.

---
+++ database/factories/ReplyFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $factory \Illuminate\Database\Eloquent\Factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(\App\Reply::class, function (Faker $faker) {
+    return [
+        'body' => $faker->paragraph,
+        'user_id' => function () {
+           return factory(\App\User::class)->create()->id;
+        },
+        'thread_id' => function () {
+            return factory(\App\Thread::class)->create()->id;
+        },
+    ];
+});
