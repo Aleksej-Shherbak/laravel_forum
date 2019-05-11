1d7025307017f104e24f5d15e1158e6f9e8a2d95
aleksej <aleksej.shherbak@yandex.ru>
2019-05-11 21:38:30 +0300

    Added relations between models, created unit test ReplayTest, that checks assertion: a reply has owner

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Reply;
 use App\Thread;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Tests\TestCase;
@@ -10,25 +11,35 @@ class ReadThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->thread = factory(Thread::class)->create();
+    }
+
     /** @test */
     public function a_user_can_see_all_threads()
     {
-        $thread = factory(Thread::class)->create();
-
         $response = $this->get(route('threads'));
 
-        $response->assertSee($thread->title);
+        $response->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_see_a_single_thread()
     {
-        $thread = factory(Thread::class)->create();
+        $response = $this->get(route('thread', $this->thread));
 
-        $response = $this->get(route('thread', $thread));
-
-        $response->assertSee($thread->title);
+        $response->assertSee($this->thread->title);
     }
 
+    /** @test */
+    public function a_user_can_read_replies_that_are_associated_with_a_thread()
+    {
+        $reply = factory(Reply::class)->create(['thread_id' => $this->thread->id]);
 
+        $this->get(route('thread', $this->thread))
+            ->assertSee($reply->body);
+    }
 }
